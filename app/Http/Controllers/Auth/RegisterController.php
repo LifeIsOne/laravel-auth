eb886d793d1460bf27aeaba2dbbe80f3feb8ee6e
LifeIsOne <didtmdgh258@gmail.com>
2025-09-19 20:59:57 +0900

    [FEAT] Laravel Lang로 한국어 유효성 검사 추가

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Rules\Password as PasswordRule;
 
 class RegisterController extends Controller
 {
+    // 회원가입 뷰 반환
     public function showRegistrationForm(){
         return view('auth.register');
+    }
+    // 유효성 검사
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required|max:16',
+            'email' => 'required|email|unique:users|max:30',
+            'password' => [new PasswordRule()],
+        ]);
     }
 }
