f9eb72c8b609faae63aa17481f000cb8b86cd59e
LifeIsOne <didtmdgh258@gmail.com>
2025-09-30 18:43:30 +0900

    [FEAT] OAuth 로그인 라우팅

--- config/services.php
+++ config/services.php
@@ -4,7 +4,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect_uri' => env('GITHUB_CALLBACK_URL'),
+        'redirect' => env('GITHUB_CALLBACK_URL'),
     ],
 
     'mailgun' => [
--- routes/auth.php
+++ routes/auth.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::controller(\App\Http\Controllers\Auth\RegisterController::class)
-    ->group(function () {
-        Route::middleware('guest')->group(function () {
-            Route::get('/register', 'showRegistrationForm')
-                ->name('register');
-            Route::post('/register', 'register');
-        });
+Route::controller(\App\Http\Controllers\Auth\RegisterController::class)->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::get('/register', 'showRegistrationForm')
+            ->name('register');
+        Route::post('/register', 'register');
     });
+});
 // 이메일 인증 라우트
 Route::controller(\App\Http\Controllers\Auth\EmailVerificationController::class)->group(function () {
     Route::name('verification.')->prefix('/email')->group(function () {
@@ -26,18 +25,28 @@ Route::controller(\App\Http\Controllers\Auth\EmailVerificationController::class)
 });
 
 // 로그인컨트롤러 라우트 그룹
-Route::controller(\App\Http\Controllers\Auth\LoginController::class)
-    ->group(function() {
-        // 로그인하지 않은 사용자만 접근 가능
-        Route::middleware('guest')->group(function () {
-            // 로그인 폼 뷰
-            Route::get('/login', 'showLoginForm')
-                ->name('login');
-            // 로그인 폼 제출
-            Route::post('/login', 'login');
-        });
-        // 로그아웃 처리 라우트
-        Route::post('/logout', 'logout')
-            ->name('logout')
-            ->middleware('auth');   // 로그인된 사용자만
+Route::controller(\App\Http\Controllers\Auth\LoginController::class)->group(function() {
+    // 로그인하지 않은 사용자만 접근 가능
+    Route::middleware('guest')->group(function () {
+        // 로그인 폼 뷰
+        Route::get('/login', 'showLoginForm')
+            ->name('login');
+        // 로그인 폼 제출
+        Route::post('/login', 'login');
     });
+    // 로그아웃 처리 라우트
+    Route::post('/logout', 'logout')
+        ->name('logout')
+        ->middleware('auth');   // 로그인된 사용자만
+});
+
+// OAuth 로그인 라우트 그룹
+Route::controller(\App\Http\Controllers\Auth\SocialLoginController::class)->group(function () {
+    // 게스트만 접근
+    Route::middleware('guest')->name('login.')->group(function() {
+        // `{procider}`로 리다이렉트
+        Route::get('/login/{provider}', 'redirect')->name('social');
+        // 로그인 후 인증 서버에서 콜백 라우트
+        Route::get('/login/{provider}/callback', 'callback')->name('social.callback');
+    });
+});
